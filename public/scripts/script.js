57e5f85b610a4cb4a110c5cb3ad16a453e29a2f6
ben-23-96 <benspeakman123@gmail.com>
2022-12-21 18:06:16 +0000

    lamdba test

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,32 +1,22 @@
-import { CognitoJwtVerifier } from "aws-jwt-verify";
-
-// Verifier that expects valid access tokens:
-const verifier = CognitoJwtVerifier.create({
-    userPoolId: "eu-west-2_OCDjnmty9",
-    tokenUse: "access",
-    clientId: "1jdksnkvu02f7qvr5l7j7ichkf",
-});
-
 var url = window.location
 console.log(url)
 var access_token = new URLSearchParams(url.search).get('id_token');
 console.log(access_token)
 
 try {
-    const payload = await verifier.verify(
-        access_token // the JWT as string
-    );
-    console.log("Token is valid. Payload:", payload);
-} catch {
-    console.log("Token not valid!");
-}
-
-pdfForm = document.querySelector('#pdfForm')
-pdfForm.addEventListener('submit', (e) => { uploadPDFToS3(e) })
-
-function uploadPDFToS3(e) {
-    e.preventDefault()
-    const formdata = new FormData(e.target);
-    file = formdata.get('pdfFile')
-    console.log(file)
+    const response = await fetch('https://ws206gr9jh.execute-api.eu-west-2.amazonaws.com/test/pdf', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: `{
+            "test": 'successful lambda progress'
+        }`
+    });
+    resStatus = await response.status;
+    console.log(resStatus)
+    console.log(response)
+} catch (error) {
+    console.error(error);
 }
