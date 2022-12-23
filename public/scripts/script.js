ebca73e7cd7f49d9797fda291235b54af1b4c350
ben-23-96 <benspeakman123@gmail.com>
2022-12-23 11:22:07 +0000

    cognito update

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,22 +1,33 @@
-var url = window.location
-console.log(url)
-var access_token = new URLSearchParams(url.search).get('id_token');
-console.log(access_token)
+var url = window.location.hash;
 
-try {
-    const response = await fetch('https://ws206gr9jh.execute-api.eu-west-2.amazonaws.com/test/pdf', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: `{
-            "test": 'successful lambda progress'
-        }`
-    });
-    resStatus = await response.status;
-    console.log(resStatus)
-    console.log(response)
-} catch (error) {
-    console.error(error);
+var access_token = new URLSearchParams(url).get('id_token');
+console.log(access_token)
+console.log(url)
+
+form = document.querySelector('#pdfForm')
+console.log(form)
+form.addEventListener('submit', (e) => { uploadPDF(e) })
+
+async function uploadPDF(e) {
+    e.preventDefault()
+    //formdata = new FormData(e.target)
+    //console.log(formdata.get('pdfFile'))
+
+    try {
+        const response = await fetch('https://ws206gr9jh.execute-api.eu-west-2.amazonaws.com/test/pdfauth', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': access_token
+            },
+            body: JSON.stringify({ test: 'testing lambda function' })
+        });
+        resStatus = await response.status;
+        console.log(resStatus)
+        console.log(response)
+        console.log(response.body)
+    } catch (error) {
+        console.error(error);
+    }
 }
